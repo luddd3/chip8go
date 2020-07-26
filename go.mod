15913a3d697a520a710150a2e3d42db2c711cafc
Mathias Lundell <lundell.mathias@gmail.com>
2020-07-26 18:18:51 +0200

    feat: add tcell for graphics (wip)

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/luddd3/chip8
 
 go 1.13
+
+require github.com/gdamore/tcell v1.3.0
--- main.go
+++ main.go
@@ -2,8 +2,46 @@ package main
 
 import (
 	"fmt"
+	"github.com/gdamore/tcell"
+	"os"
 )
 
 func main() {
-	fmt.Println("hej")
+	tcell.SetEncodingFallback(tcell.EncodingFallbackASCII)
+	s, e := tcell.NewScreen()
+	s.SetStyle(tcell.StyleDefault.
+		Foreground(tcell.ColorBlack).
+		Background(tcell.ColorWhite))
+	s.Clear()
+	if e != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", e)
+		os.Exit(1)
+	}
+	if e = s.Init(); e != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", e)
+		os.Exit(1)
+	}
+
+	s.SetStyle(tcell.StyleDefault.
+		Foreground(tcell.ColorBlack).
+		Background(tcell.ColorWhite))
+	s.Clear()
+	quit := make(chan struct{})
+	go func() {
+		for {
+			ev := s.PollEvent()
+			switch ev := ev.(type) {
+			case *tcell.EventKey:
+				switch ev.Key() {
+				case tcell.KeyEscape, tcell.KeyEnter:
+					close(quit)
+					return
+				case tcell.KeyCtrlL:
+					s.Sync()
+				}
+			case *tcell.EventResize:
+				s.Sync()
+			}
+		}
+	}()
 }
